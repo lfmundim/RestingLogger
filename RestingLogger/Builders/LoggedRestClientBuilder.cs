8ee3da48e97e94ee0932a0134162b65d84d44c25
Lucas Mundim <lucasm@take.net>
2019-01-14 18:28:37 -0200

    added new ctor

--- RestingLogger/Builders/LoggedRestClientBuilder.cs
+++ RestingLogger/Builders/LoggedRestClientBuilder.cs
@@ -20,10 +20,23 @@ namespace RestingLogger.Builders
         /// <returns>Log-enabled RestEase HttpClient for the given URL</returns>
         public virtual T BuildLoggedClient<T>(string baseUrl, ILogger logger)
         {
+            var httpClient = BuildLoggedClient<T>(new Uri(baseUrl), logger);
+
+            return httpClient;
+        }
+
+        /// <summary>
+        /// Builds a new HttpClient for the type T using the given URL and Serilog.ILogger
+        /// </summary>
+        /// <typeparam name="T">RestEase Interface for the HttpClient</typeparam>
+        /// <param name="baseUrl">Base URL used on the requests</param>
+        /// <param name="logger">Serilog.ILogger instance</param>
+        /// <returns>Log-enabled RestEase HttpClient for the given URL</returns>
+        public virtual T BuildLoggedClient<T>(Uri baseUrl, ILogger logger)
+        {
             var httpClient = new HttpClient(new RestLoggingHandler(logger))
             {
-                BaseAddress = new Uri(baseUrl),
-                
+                BaseAddress = baseUrl
             };
 
             return RestClient.For<T>(httpClient);
